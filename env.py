e2be9285e79de59ac8a7905e64d58e600ffbe450
abhay <abhayrawat2695@gmail.com>
2018-12-24 17:00:50 +0530

    added env file

---
+++ env.py
@@ -0,0 +1,9 @@
+import gym
+from gym.envs.registration import register
+from gym.envs.classic_control import rendering
+
+import numpy as np
+from numpy.linalg import norm
+from numpy.random import random
+
+from agent import Agent
